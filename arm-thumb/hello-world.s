2094b58a9d5295c395d6da0e71eb8cfb5e8568d6
FreddyJS <sebashgt@gmail.com>
2021-12-02 23:48:46 +0100

    Created Makefile

--- arm-thumb/hello-world.s
+++ arm-thumb/hello-world.s
@@ -2,17 +2,17 @@
 .global _start
 
 _start:
-    mov r0, #0x01    @ stdout
-    ldr r1, =_string @ address of the string
-    mov r2, #14      @ length of the string
-    mov r7, #0x4     @ syscall number for `write`
-    swi 0            @ execute syscall
+    mov r0, #0x01     @ stdout
+    ldr r1, =hellostr @ address of the string
+    mov r2, #14       @ length of the string
+    mov r7, #0x4      @ syscall number for `write`
+    swi 0             @ sofware interrupt
 
 _exit:
-    mov r7, #0x01
-    mov r0, #0x00
-    swi 0
+    mov r7, #0x01    @ syscall number for `exit`
+    mov r0, #0x00    @ exit status
+    swi 0            @ sofware interrupt
 
 .data
-_string:
+hellostr:
     .ascii "Hello, World!\n"
