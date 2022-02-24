98f96a977edebb19a1e43355c17b3a101fc10dd6
FreddyJS <sebashgt@gmail.com>
2022-02-24 04:34:59 +0100

    [frontend] Updated import

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
-import defaultCPU from './emulator'
+import defaultCPU from 'emulator'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
